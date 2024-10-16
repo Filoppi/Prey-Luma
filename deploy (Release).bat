73a41b9e93043515a6b6fb794bc84bee248bedf1
Filoppi <filippotarpini@hotmail.it>
2024-10-17 01:56:06 +0300

    Fixed project files

--- deploy (Release).bat
+++ deploy (Release).bat
@@ -1,3 +1,4 @@
+cd /D "%~dp0"
 taskkill /F /IM Prey.exe
 copy ".\ReShade Addon\build\x64-Release\Prey-Luma-ReShade.addon64" "C:\Program Files (x86)\Steam\steamapps\common\Prey\Binaries\Danielle\x64\Release"
 copy ".\Native Plugin\build\Release\Prey-Luma-Native.dll" "C:\Program Files (x86)\Steam\steamapps\common\Prey\Binaries\Danielle\x64\Release\Prey-Luma-Native.asi"
