3b013a558af4241b27d59eaa4e51ad1c81924069
Filoppi <filippotarpini@hotmail.it>
2024-11-10 18:53:36 +0200

    Improve UI, this is mostly unchanged but we added a couple of new empirically based changes that fit Prey's use cases, and improved handling of the alpha blend state 3

--- Data/Binaries/Danielle/x64/Release/Prey-Luma/include/Scaleform.hlsl
+++ Data/Binaries/Danielle/x64/Release/Prey-Luma/include/Scaleform.hlsl
@@ -11,31 +11,38 @@ float4 AdjustForMultiply( float4 col )
 	return lerp( float4( 1, 1, 1, 1 ), col, col.a );
 }
 
+// LUMA FT: we didn't rename this function to keep the code similar to vanilla, but it now does more
 float4 PremultiplyAlpha( float4 col )
 {
 #if !ENABLE_UI //TODOFT: some of these are still missing? In blur shaders?
 	return 0;
 #endif
 
+	// LUMA FT: this doesn't actually seem to be used by Prey, it simply sets the blend state to pre-multiplied alpha. The alpha of the background (render target) is ignored and never affects UI.
+	// The formula also didn't seem to make sense as it was pre-multiplied the alpha channel by itself too, though that's seemengly common in UI implementations, when this stuff is done manually
+	// (when doing straight alpha blending through GPUs blends, the alpha isn't multiplied by itself).
+	// We'd expect "LumaUIData.AlphaBlendState" to be "2" in this case.
+	if (cPremultiplyAlpha)
+	{
+		col *= col.a;
+	}
+
 	// LUMA FT: added optional UI linearization (with some advanced alpha modulations to emulate gamma space blends in linear space, and paper white brightness scaling).
 	// Note that Prey sometimes uses the "straight alpha" (LumaUIData.AlphaBlendState == 1) formula over a temporary/separate texture, which is then stored as pre-multiplied alpha,
 	// and then drawn later on the swapchain; though while doing so, they accidentally also multiply the alpha by itself, so if it was 0.9 in the source,
 	// it will end up being 0.9*0.9 in the pre-multiplied alpha texture (this would only maybe make sense if you keep drawing multiple layers on top of the same texture).
-	bool ForceStraightAlphaBlend = cPremultiplyAlpha >= 0.5;
-	col = ConditionalLinearizeUI(col, ForceStraightAlphaBlend);
+	col = ConditionalLinearizeUI(col, cPremultiplyAlpha);
 
-#if 0 // LUMA FT: this was needed in "Kingdom Come Deliverance" to avoid fade outs on hud tooltips causing a black after image they appear, it's not needed until proven otherwise in Prey
+#if 0 // LUMA FT: this was needed in "Kingdom Come Deliverance" to avoid fade outs on HUD tooltips causing a black after image they appear, it's not needed until proven otherwise in Prey
 	col.a = saturate(col.a);
 #endif
 
 #if TEST_UI
-	// LUMA FT: this doesn't actually seem to be used by Prey, it simply sets the blend state to pre-multiplied alpha. The alpha of the background (render target) is ignored and never affects UI.
-	// The formula also didn't seem to make sense as it was pre-multiplied the alpha channel by itself too.
 	if (cPremultiplyAlpha)
 	{
 		col.rgba = float4(2, 0, 0, 1);
 	}
 #endif
 
-	return cPremultiplyAlpha ? col * col.a : col;
+	return col;
 }
--- Data/Binaries/Danielle/x64/Release/Prey-Luma/include/UI.hlsl
+++ Data/Binaries/Danielle/x64/Release/Prey-Luma/include/UI.hlsl
@@ -10,7 +10,7 @@ cbuffer LumaUIData : register(b7)
 	// 1 Straight alpha blend: "result = (source.RGB * source.A) + (dest.RGB * (1 - source.A))" or "result = lerp(dest.RGB, source.RGB, source.A)"
 	// 2 Pre-multiplied alpha blend (alpha is also pre-multiplied, not just rgb): "result = source.RGB + (dest.RGB * (1 - source.A))"
 	// 3 Additive alpha blend (source is "Straight alpha" while destination is retained at 100%): "result = (source.RGB * source.A) + dest.RGB"
-	// 4 Additive blend (source and destination are simply summed up): result = source.RGB + dest.RGB
+	// 4 Additive blend (source and destination are simply summed up, ignoring the alpha): result = source.RGB + dest.RGB
 	uint AlphaBlendState;
 	// Modulates the alpha of the background, decreasing it to emulate background tonemapping (we don't have access to the actual background color so we can only modulate the alpha).
 	// Disabled at 0. Maxed out at 1.
@@ -19,24 +19,35 @@ cbuffer LumaUIData : register(b7)
   } LumaUIData : packoffset(c0);
 }
 
-//TODOFT3: looks good? Probably not
-// Whether to use the most theoretically mathematically accurate formulas, so the ones that actually look best (or generally better) in Prey's use case.
-#define EMPYRICAL_UI_BLENDING 0
+// Whether to use the most theoretically mathematically accurate formulas (which are generally correct in the average case), or ones specifically tailored for Prey's use cases:
+#define EMPYRICAL_UI_BLENDING_1 1
+// This is currently disabled as it still doesn't look better.
+#define EMPYRICAL_UI_BLENDING_2 0
+#define EMPYRICAL_UI_BLENDING_3 1
 
 // Guessed value of the average background color (or scene/UI brightness)
 static const float AverageUIBackgroundColorGammaSpace = 1.0 / 3.0;
 
-float UIColorIntensity(float3 Color)
+// Returns the perceptual "intensity" of the color
+float UIColorIntensity(float3 Color, bool LinearSpace = false)
 {
-#if 1 //TODOFT3: do blends by luminance (or inverse luminance) instead? (it looks about the same)
-	return GetLuminance(Color);
-#else
-	return average(Color);
+#if 1
+	float luminance = GetLuminance(LinearSpace ? Color : game_gamma_to_linear(Color));
+	return linear_to_game_gamma(luminance).x;
+#elif 1 // This is less accurate as it doesn't calculate the luminance in linear space, though it looks about identical
+	return GetLuminance(LinearSpace ? linear_to_game_gamma(Color) : Color);
+#else // This isn't the most perceptually accurate one, though again it looks about identical so it could be considered as an optimization path
+	return average(LinearSpace ? linear_to_game_gamma(Color) : Color);
 #endif
 }
 
-float4 ConditionalLinearizeUI(float4 UIColor, bool ForceStraightAlphaBlend = false)
+float4 ConditionalLinearizeUI(float4 UIColor, bool PreMultipliedAlphaByAlpha = false, bool ForceStraightAlphaBlend = false)
 {
+	// Luma FT: In this case, the game is likely drawing on scene/world interactive computers that use Scaleform UI
+	// (it's the first thing it does every frame, so as long as we are before "PostAAComposites", we know that'd be the case).
+	// They'd be drawn in gamma space and then linearized in the scene with an sRGB texture view.
+	// There's a good chance the developers developed these on a gamma 2.2 screen and thus the texture would be meant to be linearized with
+	// gamma 2.2 instead of sRGB. We can't be certain, but if we ever wanted, we could pre-apply a 2.2<->sRGB mismatch here, to visualize these UIs correctly in the world.
 	if (!LumaUIData.WritingOnSwapchain)
 	{
 #if 0 // Quick test
@@ -45,7 +56,10 @@ float4 ConditionalLinearizeUI(float4 UIColor, bool ForceStraightAlphaBlend = fal
 			UIColor = 1;
 		}
 #endif
-		return UIColor; 
+#if 0 // A WIP version of the idea mentioned above (we'd still need to make sure we don't re-apply the gamma correction on textures that are are first redrawn aside and then on the actual final render target) (this throws warning 4000?)
+		UIColor.rgb = linear_to_gamma(gamma_sRGB_to_linear(UIColor.rgb));
+#endif
+		return UIColor;
 	}
 	
 	bool gammaSpace = true;
@@ -57,7 +71,12 @@ float4 ConditionalLinearizeUI(float4 UIColor, bool ForceStraightAlphaBlend = fal
 	{
 		float3 UIColorLinearSpace = game_gamma_to_linear(UIColor.rgb);
 
-		float targetUIAlpha = safePow(UIColor.a, DefaultGamma); // Same as "gamma_to_linear(GCT_MIRROR)"
+#if EMPYRICAL_UI_BLENDING_1
+		// This looks better on average in the inventory menu (the blends with the background)
+		float targetUIAlpha = safePow(UIColor.a, pow(DefaultGamma, 0.75));
+#else
+		float targetUIAlpha = safePow(UIColor.a, DefaultGamma); // Same as "gamma_to_linear(GCT_MIRROR)", we can't modulate alpha with sRGB encoding)
+#endif
 		// This is equivalent to "game_gamma_to_linear(UIColor.rgb * UIColor.a)", despite that not being so intuitive, we can apply the same pow to either the color or the alpha to get the same result
 		float3 targetPostPreMultipliedAlphaUIColorLinearSpace = UIColorLinearSpace * targetUIAlpha;
 
@@ -74,7 +93,17 @@ float4 ConditionalLinearizeUI(float4 UIColor, bool ForceStraightAlphaBlend = fal
 		// Note that if we wanted, we could always guess that the background is mid gray (or something like that) and modulate our alpha based on that assumption,
 		// but while that would look better in some/most cases, it would look worse (possibly a lot worse) in other cases, so we prefer to do something
 		// more conservative that never looks that bad.
-		float targetBackgroundAlpha = lerp(1.0 - safePow(1.0 - UIColor.a, DefaultGamma), targetUIAlpha, saturate(UIColorIntensity(UIColor.rgb)));
+		// 
+		// The math we are trying to replicate is this:
+		// ResultColorGammaSpace = (SourceColorGammaSpace * SourceAlpha) + (BackgroundColor * (1 - SourceAlpha))
+		// But given the background is in linear space now, we need to modulate the alpha.
+		// When the source color is dark, we use (the double "1 - x" in the lerp below is because of this):
+		// ResultColorLinearSpace = (SourceColorGammaSpace^2.2 * SourceAlpha^2.2) + (BackgroundColor^2.2 * (1 - SourceAlpha)^2.2)
+		// When the source color is bright, we use:
+		// ResultColorLinearSpace = (SourceColorGammaSpace^2.2 * SourceAlpha^2.2) + (BackgroundColor^2.2 * 1 - SourceAlpha^2.2)
+		// We only have on alpha shared by the source and target color, so we "pre-divide" the pre-multiplied color by the new alpha,
+		// so that after alpha blending, it will have the value we expected it to have in the formula above.
+		float targetBackgroundAlpha = lerp(1.0 - safePow(1.0 - UIColor.a, DefaultGamma), targetUIAlpha, saturate(UIColorIntensity(UIColorLinearSpace.rgb, true)));
 #else // This alternative is simpler but always worse. Generally it's 50% accurate, and revolves around the target result when the UI color goes from black to white.
 		float targetBackgroundAlpha = UIColor.a;
 #endif
@@ -98,13 +127,21 @@ float4 ConditionalLinearizeUI(float4 UIColor, bool ForceStraightAlphaBlend = fal
 		}
 		gammaSpace = false;
 	}
-	else if (LumaUIData.AlphaBlendState == 2)
+	else if (LumaUIData.AlphaBlendState == 2 && UIColor.a != 0)
 	{
-		float3 prePreMultipliedAlphaUIColor = UIColor.a != 0 ? (UIColor.rgb / UIColor.a) : UIColor.rgb;
-		float targetUIAlpha = safePow(UIColor.a, DefaultGamma); // Same as "gamma_to_linear(GCT_MIRROR)"
-#if EMPYRICAL_UI_BLENDING // This case seems to look ~identical to vanilla in Prey
+		// Branch on whether the alpha channel was also pre-multiplied by itself.
+		// It seems to look identical independently of the value it has, so we leave it off by default,
+		// as it matches what the Scaleform code generally does (we can't find any alpha pre-multiply code, and having it off is also mathematically correct (it matches the GPU blend mode behaviour))
+		float3 prePreMultipliedAlphaUIColor = UIColor.rgb / (PreMultipliedAlphaByAlpha ? sqrt(UIColor.a) : UIColor.a);
+
+#if EMPYRICAL_UI_BLENDING_1
+		float targetUIAlpha = safePow(UIColor.a, pow(DefaultGamma, 0.75));
+#else
+		float targetUIAlpha = safePow(UIColor.a, DefaultGamma);
+#endif
+#if EMPYRICAL_UI_BLENDING_2
 		float targetBackgroundAlpha = lerp(1.0 - safePow(1.0 - UIColor.a, DefaultGamma), targetUIAlpha, saturate(UIColorIntensity(UIColor.rgb)));
-#else // Theoretically this is more "mathematically accurate" and should provide the best results possible (except it doesn't, at least in the Prey use cases)
+#else // Theoretically this is more "mathematically accurate" and should provide the best results possible across the most cases
 		float targetBackgroundAlpha = lerp(1.0 - safePow(1.0 - UIColor.a, DefaultGamma), targetUIAlpha, saturate(UIColorIntensity(prePreMultipliedAlphaUIColor)));
 #endif
 		
@@ -112,23 +149,25 @@ float4 ConditionalLinearizeUI(float4 UIColor, bool ForceStraightAlphaBlend = fal
 
 		UIColor.a = targetBackgroundAlpha;
 // This shouldn't be needed given that the UI color is pre-multiplied and already has the "perfect" alpha intensity,
-// so theoretically we'd only need to modulate the background darkening alpha, but somehow this makes the output look more accurate.
-#if EMPYRICAL_UI_BLENDING
-		if (UIColor.a != 0)
-		{
-			UIColor.rgb = prePreMultipliedAlphaUIColor * UIColor.a;
-		}
+// so theoretically we'd only need to modulate the background darkening alpha, but somehow this makes the output look more accurate in some cases.
+#if EMPYRICAL_UI_BLENDING_2
+		UIColor.rgb = prePreMultipliedAlphaUIColor * UIColor.a;
 #endif
 	}
-	// There's no need to do any gamma modulation here given that we already convert the additive source color from gamma to linear space,
-	// thus if we applied any further modulation on alpha, we'd double correct it.
-	else if (LumaUIData.AlphaBlendState == 3)
+	else if (LumaUIData.AlphaBlendState == 3 && UIColor.a != 0)
 	{
-#if 0 // Not needed until proven otherwise
+#if 0 // Not needed until proven otherwise (it looks worse at the moment)
 		// Bias towards the right result, at the cost of having a worse result if the background was too far from the guessed value
 		float3 averageBlendedColor = (UIColor.rgb * UIColor.a) + AverageUIBackgroundColorGammaSpace;
 		float3 averageLinearBlendedColor = linear_to_game_gamma((game_gamma_to_linear(UIColor.rgb) * UIColor.a) + safePow(AverageUIBackgroundColorGammaSpace, DefaultGamma));
-		UIColor.rgb += safeDivision(averageBlendedColor - averageLinearBlendedColor, UIColor.a, 0);
+		UIColor.rgb += (averageBlendedColor - averageLinearBlendedColor) / UIColor.a;
+#endif
+
+#if EMPYRICAL_UI_BLENDING_3
+		// Theoretically, in this case, there's no need to do any gamma modulation given that we already convert the additive source color from gamma to linear space,
+		// thus if we applied any further modulation on alpha, we'd double correct it, but... somehow doing this makes it look like vanilla (especially if we are blending on a black background)
+		// so for now we leave it in (this case isn't often used in Prey, it's only in the inventory menu pretty much)
+		UIColor.a = safePow(UIColor.a, DefaultGamma);
 #endif
 	}
 	else if (LumaUIData.AlphaBlendState == 4)
